0e82f6d015d3f275a429505ba253d5f498c2667c
Nick Nisi <nick.nisi@workos.com>
2025-03-24 11:24:22 -0500

    vuln example

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,9 @@
 import { authkitMiddleware } from "@workos-inc/authkit-nextjs";
 
-export default authkitMiddleware();
+export default authkitMiddleware({ middlewareAuth: {
+  unauthenticatedPaths: [],
+  enabled: true,
+}});
 
 // Match against the pages
-export const config = { matcher: ["/", "/account/:path*", "/api/:path*"] };
+export const config = { matcher: ["/", "/account/:path*", "/api/:path*", "/api/test"] };
