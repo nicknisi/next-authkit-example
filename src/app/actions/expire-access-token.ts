db5f03dcd888d20aea1f3413d82e33ff5e91bf19
Nick Nisi <nick.nisi@workos.com>
2025-03-28 10:23:05 -0500

    wip

--- src/app/actions/expire-access-token.ts
+++ src/app/actions/expire-access-token.ts
@@ -4,7 +4,65 @@ import { sealData, unsealData } from "iron-session";
 import { decodeJwt, SignJWT } from "jose";
 import { cookies } from "next/headers";
 import { TextEncoder } from "util";
-import { getCookieOptions } from "@workos-inc/authkit-nextjs";
+
+interface CookieOptions {
+  path: '/';
+  httpOnly: true;
+  secure: boolean;
+  sameSite: 'lax' | 'strict' | 'none';
+  maxAge: number;
+  domain: string | undefined;
+}
+
+type ValidSameSite = CookieOptions['sameSite'];
+
+function assertValidSamSite(sameSite: string): asserts sameSite is ValidSameSite {
+  if (!['lax', 'strict', 'none'].includes(sameSite.toLowerCase())) {
+    throw new Error(`Invalid SameSite value: ${sameSite}`);
+  }
+}
+
+const {WORKOS_REDIRECT_URI = '', WORKOS_COOKIE_SAMESITE, WORKOS_COOKIE_MAX_AGE, WORKOS_COOKIE_DOMAIN} = process.env;
+
+function getCookieOptions(): CookieOptions;
+function getCookieOptions(redirectUri?: string | null): CookieOptions;
+function getCookieOptions(redirectUri: string | null | undefined, asString: true, expired?: boolean): string;
+function getCookieOptions(
+  redirectUri: string | null | undefined,
+  asString: false,
+  expired?: boolean,
+): CookieOptions;
+function getCookieOptions(
+  redirectUri?: string | null,
+  asString?: boolean,
+  expired?: boolean,
+): CookieOptions | string;
+function getCookieOptions(
+  redirectUri?: string | null,
+  asString: boolean = false,
+  expired: boolean = false,
+): CookieOptions | string {
+  const url = new URL(redirectUri || WORKOS_REDIRECT_URI);
+  const sameSite = WORKOS_COOKIE_SAMESITE || 'lax';
+  assertValidSamSite(sameSite);
+  const secure = sameSite.toLowerCase() === 'none' ? true : url.protocol === 'https:';
+
+  const maxAge = expired ? 0 : WORKOS_COOKIE_MAX_AGE ? parseInt(WORKOS_COOKIE_MAX_AGE, 10) : 60 * 60 * 24 * 400;
+
+  return asString
+    ? `Path=/; HttpOnly; Secure=${secure}; SameSite="${sameSite}"; Max-Age=${maxAge}; Domain=${WORKOS_COOKIE_DOMAIN || ''}`
+    : {
+        path: '/',
+        httpOnly: true,
+        secure,
+        sameSite,
+        // Defaults to 400 days, the maximum allowed by Chrome
+        // It's fine to have a long cookie expiry date as the access/refresh tokens
+        // act as the actual time-limited aspects of the session.
+        maxAge,
+        domain: WORKOS_COOKIE_DOMAIN || '',
+      };
+}
 
 // Replace with the actual cookie password from your .env file
 const COOKIE_PASSWORD = process.env.WORKOS_COOKIE_PASSWORD || "";
